415bb2991f3da4ab567cd277e812b3bb7c997132
markjones-msft <54671949+markjones-msft@users.noreply.github.com>
2019-09-12 14:18:06 +0100

    RC

--- Check Subscription for SQL Servers.ps1
+++ Check Subscription for SQL Servers.ps1
@@ -6,18 +6,17 @@ $Script = "C:\Code\CheckSQLStatus.ps1"
 
 $CSVPath = (Read-Host "Enter the Path to CSV Output File")
 $CSVPath = "C:\Code\SQLStatus.csv"
-$CSV=""
+
+Write-host -BackgroundColor Black -ForegroundColor Yellow "The following servers are found in this subscription:"
+write-host (Get-Azvm).Name
 
 #########################################################################################
 # Get All VMs in the subscription ad check for SQL based on Name and Registry
 #########################################################################################
 If((test-path $CSVPath) -eq "True") {del $CSVPath}
-Add-Content -Path $CSVPath  -Value '"ResourceGroup","ServerName","HasSQLInstalled","Verified"'
+Add-Content -Path $CSVPath  -Value '"ResourceGroup","ServerName","HasSQLInstalled","ServerChecked"'
 
-$Servers = (get-azvm)
-
-Write-host -BackgroundColor Black -ForegroundColor Yellow "The following servers are found in this subscription:"
-get-azvm | SELECT ResourceGroupName, Name
+$Servers = (Get-Azvm)
 
 if (($Response = Read-host "Would you like to check all servers or just those containing the word ""SQL"". (Press A to Check for ALL or any other key to just check Servers Contianing SQL.") -eq "A") 
     {$Response = "All"}
@@ -31,12 +30,14 @@ ForEach ($Server in $Servers)
                                         
                 # Test for Registry Path
                 $Status= Invoke-AzVMRunCommand -ResourceGroupName $Server.ResourceGroupName -Name $Server.Name -CommandId RunPowerShellScript -ScriptPath $Script 
-                write-host $Server.Name ", SQL Installed: " $Status.value.message
+                $StatusFlag = $Status.value.message
+
+                write-host $Server.Name ", SQL Installed: $StatusFlag"
 
                 [PSCustomObject]@{'ResourceGroup' = $Server.ResourceGroupName
                                 'ServerName' = $Server.Name
-                                'HasSQLInstalled' = $Status.value.message
-                                'Verified' = "yes"
+                                'HasSQLInstalled' = "$StatusFlag"
+                                'ServerChecked' = "Yes"
                     
                 } | Export-Csv -Path $CSVPath -Append
                  
@@ -48,8 +49,8 @@ ForEach ($Server in $Servers)
 
                 [PSCustomObject]@{'ResourceGroup' = $Server.ResourceGroupName
                     'ServerName' = $Server.Name
-                    'HasSQLInstalled' = "Unlikely - No SQL in Servername"
-                    'Verified' = "No"
+                    'HasSQLInstalled' = "Unkown"
+                    'ServerChecked' = "No"
                     } | Export-Csv -Path $CSVPath -Append
         }
 
